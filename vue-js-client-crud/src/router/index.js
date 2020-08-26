abb1e79a42a6c730bafb9febc19f6e5f12f8f09d
Matien <ms@elbdev.de>
2020-08-26 11:37:30 +0200

    scss config

---
+++ vue-js-client-crud/src/router/index.js
@@ -0,0 +1,21 @@
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import HelloWorld from "@/components/HelloWorld";
+
+Vue.use(VueRouter)
+
+const routes = [
+    {
+        path: '/',
+        name: 'HelloWorld',
+        component: HelloWorld
+    }
+]
+
+const router = new VueRouter({
+    mode: 'history',
+    routes
+})
+
+export default router
+
