dd614b38b49edd78cd8ddaf7fc05141f3ce63dd1
Matien <ms@elbdev.de>
2020-11-09 13:28:19 +0100

    navigation toggle fucntion added

--- vue-js-client-crud/src/layouts/Navigation.vue
+++ vue-js-client-crud/src/layouts/Navigation.vue
@@ -1,19 +1,29 @@
 <template>
   <header class="page-header">
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-      <a class="navbar-brand" href="#">Navbar</a>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup"
-              aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-        <div class="navbar-nav">
-          <a class="nav-link active" href="/home">Home<span class="sr-only">(current)</span></a>
-          <a class="nav-link" href="/weekly">Meal Table</a>
-          <a class="nav-link" href="/meal">Meal List</a>
-        </div>
-      </div>
-    </nav>
+    <div>
+      <b-navbar toggleable="lg" variant="light">
+        <b-navbar-brand href="/home">FOOD</b-navbar-brand>
+        <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+        <b-collapse id="nav-collapse" is-nav>
+          <b-navbar-nav>
+            <b-nav-item href="/home">Home</b-nav-item>
+            <b-nav-item href="/weekly">Meal-Tables</b-nav-item>
+            <b-nav-item href="/meal">Meal-List</b-nav-item>
+          </b-navbar-nav>
+          <b-navbar-nav class="ml-auto">
+            <b-nav-form>
+            </b-nav-form>
+            <b-nav-item-dropdown right>
+              <template #button-content>
+               <img class="trash-can" src="../../src/assets/images/user.svg" alt="user">
+              </template>
+              <b-dropdown-item href="/admin">Profile</b-dropdown-item>
+              <b-dropdown-item href="/home">Sign Out</b-dropdown-item>
+            </b-nav-item-dropdown>
+          </b-navbar-nav>
+        </b-collapse>
+      </b-navbar>
+    </div>
   </header>
 
 </template>
@@ -24,9 +34,12 @@ export default {
 }
 </script>
 
-<style scoped>
+<style scoped lang="scss">
 .navbar-brand {
-  color: #DB037B !important;
+  color: #DB037B !important ;
+}
+.nav-link {
+  color: grey;
 }
 
 </style>
