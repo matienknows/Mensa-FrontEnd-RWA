642923692a93b52fa9578ec46ed83b69678acd97
Matien <ms@elbdev.de>
2020-11-18 12:38:05 +0100

    login style modification

--- vue-js-client-crud/src/components/LoginComponent.vue
+++ vue-js-client-crud/src/components/LoginComponent.vue
@@ -1,20 +1,27 @@
 <template>
-  <form>
-    <div class="form-group col-lg-4 offset-lg-4 text-center">
-      <label for="username">Username</label>
-      <input type="text" class="form-control" id="username" v-model="username" placeholder="Enter username">
-      <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+  <section class="login">
+    <div class="container">
+      <div class="login-logo text-center">
+        <img class="user" src="../../src/assets/images/user.svg" alt="user">
+      </div>
+      <form>
+        <div class="form-group col-lg-4 offset-lg-4 text-center">
+          <label for="username">Username</label>
+          <input type="text" class="form-control" id="username" v-model="username" placeholder="Enter username">
+          <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+        </div>
+        <div class="form-group col-lg-4 offset-lg-4 text-center">
+          <label for="password">Password</label>
+          <input type="password" class="form-control" id="password" v-model="password" placeholder="Password">
+        </div>
+        <div class="button row justify-content-center">
+          <button type="button" class="btn btn-outline-info col-6" v-on:click="proveAuthenticaiton" tag="button">
+            Login
+          </button>
+        </div>
+      </form>
     </div>
-    <div class="form-group col-lg-4 offset-lg-4 text-center">
-      <label for="password">Password</label>
-      <input type="password" class="form-control" id="password" v-model="password" placeholder="Password">
-    </div>
-    <div class="button row justify-content-center">
-      <button type="button" class="btn btn-outline-info col-6" v-on:click="proveAuthenticaiton" tag="button">
-        Login
-      </button>
-    </div>
-  </form>
+  </section>
 </template>
 
 <script>
@@ -50,7 +57,13 @@ export default {
   },
 }
 </script>
-
-
 <style scoped lang="scss">
+
+.login-logo {
+  margin: 45px 0 45px 0;
+  img {
+    width: 25px;
+  }
+}
+
 </style>
